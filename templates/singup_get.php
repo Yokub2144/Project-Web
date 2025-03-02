33ded2e3bf31a149b93a20c48007ad49ca843cfb
Yokub2144 <Suwat.Paomong@gmail.com>
2025-03-02 13:06:28 +0700

    pull

--- templates/singup_get.php
+++ templates/singup_get.php
@@ -1,11 +1,17 @@
 <section>
         <div class="form-container sign-in">
-            <form>
+            <form action="/signin" method="post">
                 <h1>Sign In</h1>
-                <input type="email" placeholder="Email">
-                <input type="password" placeholder="Password">
-                <a href="#">Forget Your Password?</a>
-                <button>Sign In</button>
+                <input type="text" name="name" placeholder="Name">
+                <input type="email" name="email" placeholder="Email">
+                <input type="password" name="password" placeholder="Password">
+                <input type="text" name="phone" placeholder="Phone">
+                <input type="text" name="Gender" placeholder="Gender">
+                <input type="text" name="Age" placeholder="Age">
+                <input type="text" name="ImageProfileURL"  placeholder="ImageProfileURL">
+                <a href="/updatepassword">Forget Your Password?</a>
+                
+                <input type="submit" value="Sign In">
             </form>
         </div>
     <script src="script.js"></script>
