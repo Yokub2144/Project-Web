445e620c68a6c0201a6cc163f85a3e25a5222897
Jatuthep2005 <66011212008@msu.ac.th>
2025-03-08 02:10:43 +0700

    Update singup_get.php

--- templates/singup_get.php
+++ templates/singup_get.php
@@ -1,25 +1,31 @@
 <section style="display: flex; justify-content: center; align-items: center; min-height: 100vh;">
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
+
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
         <title>Modern Login Page | AsmrProg</title>
-        <link rel="stylesheet" href="style_singup.css">
-        
-        
+        <link rel="stylesheet" href="/CSS/style_singup.css">
+
+
     </head>
-    
+
     <body>
         <div class="container_singup" id="container_singup">
             <div class="form-container_singup sign-in">
-                <form  action="/singup" method="post">
+                <form action="/singup" method="post">
                     <input type="text" name="name" placeholder="Name">
                     <input type="email" id="email" name="email" placeholder="Email">
                     <input type="password" id="password" name="password" placeholder="Password">
                     <input type="text" name="phone" placeholder="Phone">
-                    <input type="text" name="gender" placeholder="Gender">
+                    <select name="gender" required>
+                        <option value="">Gender</option>
+                        <option value="Male">ชาย</option>
+                        <option value="Female">หญิง</option>
+                        <option value="Other">อื่นๆ</option>
+                    </select>
                     <input type="number" name="age" placeholder="Age">
                     <input type="file" name="imageProfileURL" placeholder="ImageProfileURL">
                     <input style="background-color: #55065c; color:white" type="submit" value="Signup">
