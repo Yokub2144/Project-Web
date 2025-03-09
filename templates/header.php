bc5b9263573961a4a94f4b4a2d138631479f2aa6
Jatuthep2005 <66011212008@msu.ac.th>
2025-03-09 22:48:49 +0700

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -7,7 +7,7 @@
     <title>Active Zone</title>
     <link rel="icon" href="img/logo.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-     <link rel="stylesheet" href="/CSS/Style_header.css"> -->
+     <link rel="stylesheet" href="/CSS/Style_header.css">
     
 </head>
 
